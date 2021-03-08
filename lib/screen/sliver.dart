5e7b15099761aa79a9a1e7ebeea54312736085ad
oszkarpap <scaevola.c@gmail.com>
2021-03-08 11:53:09 +0100

    first code commit

--- lib/screen/sliver.dart
+++ lib/screen/sliver.dart
@@ -5,61 +5,53 @@ class SliverPage extends StatefulWidget {
   SliverAppBarState createState() => SliverAppBarState();
 }
 
-class SliverAppBarState extends State<SliverPage>  {
+class SliverAppBarState extends State<SliverPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              leading: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: IconButton(
-                    icon: Icon(Icons.arrow_back_sharp),
-                    onPressed: () {
-                     Navigator.pop(context);
-                    }
-                ),
-              ),
-              expandedHeight: 220.0,
-              floating: true,
-              pinned: true,
-              snap: true,
-              elevation: 50,
-              backgroundColor: Colors.blueGrey[600],
-              flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text('Title',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Image.network(
-                    'https://images.pexels.com/photos/443356/pexels-photo-443356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    fit: BoxFit.cover,
-                  )
-              ),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                  icon: Icon(Icons.arrow_back_sharp),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }),
             ),
-            new SliverList(
-                delegate: new SliverChildListDelegate(_buildList(50))
-            ),
-          ],
-        ),
+            expandedHeight: 220.0,
+            floating: true,
+            pinned: true,
+            snap: true,
+            elevation: 50,
+            backgroundColor: Colors.blueGrey[600],
+            flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text('Title',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    )),
+                background: Image.network(
+                  'https://images.pexels.com/photos/443356/pexels-photo-443356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                  fit: BoxFit.cover,
+                )),
+          ),
+          new SliverList(delegate: new SliverChildListDelegate(_buildList(50))),
+        ],
       ),
     );
   }
 
   List _buildList(int count) {
-    List<Widget> listItems = List();
+    List<Widget> listItems = [];
 
     for (int i = 0; i < count; i++) {
-      listItems.add(new Padding(padding: new EdgeInsets.all(20.0),
-          child: new Text(
-              'Item ${i.toString()}',
-              style: new TextStyle(fontSize: 25.0)
-          )
-      ));
+      listItems.add(new Padding(
+          padding: new EdgeInsets.all(20.0),
+          child: new Text('Item ${i.toString()}',
+              style: new TextStyle(fontSize: 25.0))));
     }
 
     return listItems;
